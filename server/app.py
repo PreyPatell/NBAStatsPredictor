912e06356b794fadb9e08f4ed862ffc5c7ae6635
ppate454 <84814013+PreyPatell@users.noreply.github.com>
2024-03-29 22:10:47 -0400

    feat: reset model for stats

--- server/app.py
+++ server/app.py
@@ -3,7 +3,15 @@ from flask import request, jsonify
 
 app = Flask(__name__)
 
-# Testing API endpoint
+# Testing API Endpoint
 @app.get("/api/test-api")
 def test_api():
     return jsonify({"message": "API Working"})
+
+# Post NBA Player Name
+
+# Get NBA Player Career Stats
+
+# Get NBA Player Past 10 Game Log
+
+# Get Prediction for Next 5 Games Through Model
